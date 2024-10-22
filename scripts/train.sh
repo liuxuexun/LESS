86dc4edd931c391e9f16b56e19182168ee4c1b66
mellody11 <1846960330@qq.com>
2024-10-22 09:10:39 +0800

    modify train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,7 @@
-TORCH_DISTRIBUTED_DEBUG=INFO CUDA_VISIBLE_DEVICES=1 PYTHONPATH=./ OMP_NUM_THREADS=4 python -m torch.distributed.launch \
-    --nproc_per_node 1 --master_port 1112 \
+TORCH_DISTRIBUTED_DEBUG=INFO CUDA_VISIBLE_DEVICES=0,1 PYTHONPATH=./ OMP_NUM_THREADS=4 python -m torch.distributed.launch \
+    --nproc_per_node 2 --master_port 1111 \
     scripts/train.py \
     --learning_rate 0.0001 \
     --tag "no word_feature (offical)" \
     --batch_size 7
-    # --use_checkpoint /data/liuxuexun/code/NIPS/LESS/outputs/2024-10-19_14-25-22/checkpoint_last.pth
 
