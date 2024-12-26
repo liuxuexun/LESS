25ee823a40dcd3e0cb92ee91e14329b6ba028ab4
liuxuexun <1846960330@qq.com>
2024-12-26 15:18:10 +0800

    del

--- scripts/train.sh
+++
@@ -1,7 +0,0 @@
-TORCH_DISTRIBUTED_DEBUG=INFO CUDA_VISIBLE_DEVICES=0,1 PYTHONPATH=./ OMP_NUM_THREADS=4 python -m torch.distributed.launch \
-    --nproc_per_node 2 --master_port 1111 \
-    scripts/train.py \
-    --batch_size 7 \
-    --tag "" \
-    
-
